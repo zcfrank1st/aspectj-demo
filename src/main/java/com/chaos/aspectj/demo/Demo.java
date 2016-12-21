057972087348a5649b24cf51d8d92da745e51fbb
zcfrank1st <zcfrank1st@sina.com>
2016-12-21 17:05:54 +0800

    first commit

---
+++ src/main/java/com/chaos/aspectj/demo/Demo.java
@@ -0,0 +1,14 @@
+package com.chaos.aspectj.demo;
+
+/**
+ * Created by zcfrank1st on 21/12/2016.
+ */
+public class Demo {
+    public void hello () {
+        System.out.println("demo");
+    }
+
+    public static void main(String[] args) {
+        new Demo().hello();
+    }
+}
