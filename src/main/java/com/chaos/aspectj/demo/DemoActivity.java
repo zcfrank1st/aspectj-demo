a21ae332ba28ddd3d4fef6229da245510aaa1c2e
zcfrank1st <zcfrank1st@sina.com>
2016-12-23 14:03:27 +0800

    add

--- src/main/java/com/chaos/aspectj/demo/DemoActivity.java
+++ src/main/java/com/chaos/aspectj/demo/DemoActivity.java
@@ -1,7 +1,10 @@
 package com.chaos.aspectj.demo;
 
+import android.app.Activity;
+
 /**
  * Created by zcfrank1st on 23/12/2016.
  */
-public class DemoActivity {
+public class DemoActivity extends Activity {
+
 }
--- src/main/java/com/chaos/aspectj/demo/Main.java
+++ src/main/java/com/chaos/aspectj/demo/Main.java
@@ -7,6 +7,6 @@ import android.app.Activity;
  */
 public class Main {
     public static void main(String[] args) {
-        new Activity().hello();
+        new DemoActivity().hello();
     }
 }
