3c3dce184729755c224bd327f9221a1d4e48f556
zcfrank1st <zcfrank1st@sina.com>
2016-12-23 11:22:18 +0800

    update plugin-version

---
+++ src/main/java/android/app/Activity.java
@@ -0,0 +1,10 @@
+package android.app;
+
+/**
+ * Created by zcfrank1st on 23/12/2016.
+ */
+public class Activity {
+    public void hello () {
+        System.out.println("haha");
+    }
+}
--- src/main/java/com/chaos/aspectj/demo/Main.java
+++ src/main/java/com/chaos/aspectj/demo/Main.java
@@ -1,10 +1,12 @@
 package com.chaos.aspectj.demo;
 
+import android.app.Activity;
+
 /**
  * Created by zcfrank1st on 22/12/2016.
  */
 public class Main {
     public static void main(String[] args) {
-        new Demo().hello();
+        new Activity().hello();
     }
 }
